58df2465e2334e30493f49cbf04d1c166f79f7b7
SilvaTorosyan <tester@Mesrops-Mac-mini.local>
2018-08-30 19:33:06 +0400

    first commit

---
+++ PhoneBook/Managers/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  PhoneBook
+//
+//  Created by Tester on 8/30/18.
+//  Copyright © 2018 aaa. All rights reserved.
+//
+
+import Foundation
---
+++ PhoneBook/UI/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  PhoneBook
+//
+//  Created by Tester on 8/30/18.
+//  Copyright © 2018 aaa. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
